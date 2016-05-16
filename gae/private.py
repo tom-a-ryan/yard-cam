fbb9074549b01759eaeddff0e10c6f92392639a7
tom-a-ryan <tom.a.ryan@gmail.com>
2016-05-15 21:05:33 -0700

    anonymize private.py

--- gae/private.py
+++ gae/private.py
@@ -4,19 +4,15 @@
 # allowed users, authenticated by google
 # not very scalable and should be moved to data store
 
-white_list = ['tom.a.ryan@gmail.com',
-              'thos.a.ryan@gmail.com',
-              'maylin.ryan@gmail.com',
-              'ryan.christine@gmail.com',
-              'ryan.stephanie@gmail.com'
+white_list = ['email@gmail.com'
               ]
 
 # ised by Flask session
-flask_secret = 'My Flask Application Secret'
+flask_secret = 'Flask Application Secret'
 
 # internal to main app, not GAE admin
 # admin can clear images, etc.
 
-admin_name = 'tom'
-admin_password = 'ryan'
+admin_name = 'admin'
+admin_password = 'change_me'
 
