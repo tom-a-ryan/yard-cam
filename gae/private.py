3eb616fdd4d3d198dc4a87dcf97bc764d8cdf599
tom-a-ryan <tom.a.ryan@gmail.com>
2016-05-15 21:23:55 -0700

    Tweak spelling and line ordering

--- gae/private.py
+++ gae/private.py
@@ -1,18 +1,16 @@
-
 # private entries for gae flask main app
 
-# allowed users, authenticated by google
-# not very scalable and should be moved to data store
+# used by Flask session, replace with a different secret
+flask_secret = 'Flask Application Secret'
 
+
+# These should probably be moved to a data store
+
+# allowed users, authenticated by google
 white_list = ['email@gmail.com'
               ]
 
-# used by Flask session, replaxe with a different secret
-flask_secret = 'Flask Application Secret'
-
 # internal to main app, not GAE admin
 # admin can clear images, etc.
-
 admin_name = 'web_admin'
 admin_password = 'change_me'
-
