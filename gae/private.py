4324d9c1e4a71f6a738c858e2dae74f41f9d8ca0
tom-a-ryan <tom.a.ryan@gmail.com>
2016-06-03 15:25:15 -0700

    add local repository safe update hints

--- gae/private.py
+++ gae/private.py
@@ -1,5 +1,10 @@
-# private entries for gae flask main.py app
+# private entries imported by gae flask main.py app
 # these need to be changed before deployment
+# this file is in .gitignore`
+# but to be extra sure to avoid accidentally sending changes back to a repository
+# execute something like
+#    git update-index --assume-unchanged gae/private.py
+# after an edit
 
 # used by Flask session, replace with a different secret
 flask_secret = 'Flask Application Secret'
