d93979c1969037a0f33d432f1c706dca68e10500
tom-a-ryan <tom.a.ryan@gmail.com>
2016-06-03 14:42:39 -0700

    reformat and simplify

--- gae/private.py
+++ gae/private.py
@@ -1,14 +1,12 @@
 # private entries for gae flask main app
+# these need to be changed before deployment
 
 # used by Flask session, replace with a different secret
 flask_secret = 'Flask Application Secret'
 
 
-# These should probably be moved to a data store
-
 # allowed users, authenticated by google
 white_list = ['tom.a.ryan@gmail.com'
-              ]
 
 # internal to main app, not GAE admin
 # admin can clear images, etc.
